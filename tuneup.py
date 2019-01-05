f5e8553d6af8fc93954da6976ab92fcbb9cba4ec
Bob Hunt <bob.hunt.email@gmail.com>
2019-01-04 20:44:22 -0500

    returning the lowest average runtime.

--- tuneup.py
+++ tuneup.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 """Tuneup assignment"""
 
-__author__ = "???"
+__author__ = "bomazani"
 
 import cProfile
 import pstats
+import timeit
 
 
 def profile(func):
@@ -17,7 +18,10 @@ def read_movies(src):
     """Read a list of movie titles"""
     print('Reading file: {}'.format(src))
     with open(src, 'r') as f:
-        return f.read().splitlines()
+        if len(src) != 0:
+            return f.read().splitlines()
+        else:
+            return
 
 
 def is_duplicate(title, movies):
@@ -50,6 +54,15 @@ def main():
     print('Found {} duplicate movies:'.format(len(result)))
     print('\n'.join(result))
 
+# print(timeit.timeit('''def main():
+#     result = find_duplicate_movies('movies.txt')
+#     print('Found {} duplicate movies:'.format(len(result)))
+#     print('\n'.join(result))''', number = 100))
+
 
 if __name__ == '__main__':
-    main()
+    import timeit
+    times = (timeit.repeat(
+        "main()", setup="from __main__ import main", repeat=3, number=3))
+    print(times)
+    print('Shortest avg runtime is {}.'.format(min(times)/3))
